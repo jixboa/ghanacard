10485111925a4ff80f4a7392d318cbdaaee7ca97
Aboagye Gyamfi <gyesboa@gmail.com>
2023-01-16 22:42:10 +0000

    Getting Back

--- src/api/root.js
+++ src/api/root.js
@@ -2,16 +2,16 @@ import axios from "axios";
 
 export const BASE_URL = "http://localhost:5000/api";
 export const IP_URL = "https://jix-todo-app.herokuapp.com/api";
-export const TEST_URL = "https://api-test.maximnyansa.com/api/v1";
 
 const root = () => {
   return axios.create({
     baseURL: IP_URL,
     withCredentials: true,
     headers: {
-      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Origin": "https://gapghanacard.netlify.app",
       Accept: "application/json",
       "Content-Type": "application/json",
+      "Access-Control-Allow-Methods": "POST, GET, OPTION, DELETE, PUT",
       "Access-Control-Allow-Headers":
         "Origin, X-Requested-With, Content-Type, Accept",
     },
