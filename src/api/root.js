9dfb5ba65f964269a9cf5e6211562c0d6d61c1fe
Aboagye Gyamfi <gyesboa@gmail.com>
2023-02-01 07:03:54 +0000

    Git host

--- src/api/root.js
+++ src/api/root.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const BASE_URL = "http://localhost:5000/api";
 export const IP_URL = "https://jix-todo-app.herokuapp.com/api";
 export const AWS_IP =
-  "http://ghanacardlinkage-env.eba-qya8a8pr.us-east-1.elasticbeanstalk.com/api";
+  "http://ghcardserver-env.eba-kearpmr8.us-east-1.elasticbeanstalk.com/api";
 
 const root = () => {
   return axios.create({
